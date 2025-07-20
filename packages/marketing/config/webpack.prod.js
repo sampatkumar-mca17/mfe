5773ba895588e4fe71792a026c571c8fb40f1453
sampat <sampat.kumar@aquera.com>
2025-07-20 17:52:05 +0530

    created container deploy scrip

---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,23 @@
+const {merge} = require('webpack-merge');
+const commonConfig = require('./webpack.common');
+const moduleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const packageJson = require('../package.json');
+const domain = process.env.PRODUCTION_DOMAIN;
+const prodConfig = {
+    mode:'production',
+    output:{
+        filename:'[name].[contenthash].js',
+    },
+    plugins:[
+        new moduleFederationPlugin({
+            name:'marketing',
+            filename:'remoteEntry.js',
+            exposes:{
+                './MarketingApp': './src/bootstrap'
+            },
+            shared:packageJson.dependencies
+        })
+    ]
+}
+
+module.exports = merge(commonConfig,prodConfig);
